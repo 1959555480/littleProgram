6f2ea1a0f1b2ef2b37de8f205961045c691e8ef8
yyx <1959555480@qq.com>
2020-05-19 18:12:58 +0800

    添加数据

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -2,6 +2,9 @@ Page({
   data: {
     items: [
       {
+        value: 'All', name: 'All⭐',
+      },
+      {
         value: 'six', name: '6⭐',
       },
       {
@@ -31,9 +34,89 @@ Page({
         value: 'top', name: '高级资深干员',
       },
     ],
+    positions: [
+      {
+        value: 'remote', name: '远程位',
+      },
+      {
+        value: 'close', name: '近战位',
+      }
+    ],
+    types: [
+      {
+        value: 'roles_1', name: '先锋干员',
+      },
+      {
+        value: 'roles_2', name: '狙击干员',
+      },
+      {
+        value: 'roles_3', name: '医疗干员',
+      },
+      {
+        value: 'roles_4', name: '术师干员',
+      },
+      {
+        value: 'roles_5', name: '近卫干员',
+      },
+      {
+        value: 'roles_6', name: '重装干员',
+      },
+      {
+        value: 'roles_7', name: '辅助干员',
+      },
+      {
+        value: 'roles_8', name: '特种干员',
+      }
+    ],
+    terms: [
+      {
+        value: 'terms_1', name: '治疗',
+      },
+      {
+        value: 'terms_2', name: '支援',
+      },
+      {
+        value: 'terms_3', name: '输出',
+      },
+      {
+        value: 'terms_4', name: '群攻',
+      },
+      {
+        value: 'terms_5', name: '减速',
+      },
+      {
+        value: 'terms_6', name: '生存',
+      },
+      {
+        value: 'terms_7', name: '防护',
+      },
+      {
+        value: 'terms_8', name: '削弱',
+      },
+      {
+        value: 'terms_9', name: '位移',
+      },
+      {
+        value: 'terms_10', name: '控场',
+      },
+      {
+        value: 'terms_11', name: '爆发',
+      },
+      {
+        value: 'terms_12', name: '召唤',
+      },
+      {
+        value: 'terms_13', name: '快速复活',
+      },
+      {
+        value: 'terms_14', name: '费用回复',
+      },
+      {
+        value: 'terms_15', name: '支援机械',
+      },
+    ],
   },
   clickCheckBox(e) {
     console.log(e)
-    // this.setDa
   },
 })
